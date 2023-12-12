877703fb46879e1f3710aab7f295e0821b7c286f
gyugeladamm <gyugeladamzoltan@turr.hu>
2023-12-12 08:38:37 +0100

    szamolas

--- Dolgozóképesség.py
+++ Dolgozóképesség.py
@@ -1,3 +1,10 @@
 
 
 bekeres=int(input("Hány éves vagy: "))
+
+if bekeres<=14:
+    print("Ebben a korban még nem dolgozhatsz")
+elif 15<=bekeres<=18:
+    print("Korlátozottan dolgozóképes vagy")
+else:
+    print("Teljes mértékben munkaképes vagy.") 
